c72a319765279582d161672d79088de61fdcc529
baizura <baizura1996@gmail.com>
2023-06-29 22:54:50 +0800

    intial
    setup supabase

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Homepage from './pages/homepage';
+import SignIn from './pages/signin';
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage/>
+      {/* <SignIn/> */}
+    </div>
+  );
+}
+
+export default App;
