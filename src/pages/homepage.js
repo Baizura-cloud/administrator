aab1f703c7655338b980c35c0627cbaa9fc83b98
baizura <baizura1996@gmail.com>
2023-07-01 22:34:44 +0800

    pull data + homepage update

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,19 +1,41 @@
 import * as React from 'react';
+import { useEffect, useState } from 'react';
 import supabase from '../config/supabaseClient';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import ResponsiveAppBar from './subpages/appbar';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const defaultTheme = createTheme();
 
 export default function Homepage() {
+  const [fetchError, setFetchError] = useState(null)
+  const [staff, setStaff] = useState(null)
+
+  useEffect(() => {
+    const fetchStaff = async () =>{
+      const {data, error} = await supabase.from('staff').select()
+
+      if(error){
+        setFetchError('erorr fetching staff data')
+        console.log(error)
+        setStaff(null)
+      }
+
+      if(data){
+        setStaff(data)
+        console.log(data)
+        setFetchError(null)
+      }
+    }
+
+    fetchStaff()
+  },[])
+
   return (
     <ThemeProvider theme={defaultTheme}>
-      <CssBaseline />
+      <ResponsiveAppBar/>
         <Box
           sx={{
             bgcolor: 'background.paper',
@@ -33,19 +55,14 @@ export default function Homepage() {
               Album layout
             </Typography>
             <Typography variant="h5" align="center" color="text.secondary" paragraph>
-              Something short and leading about the collection below—its contents,
-              the creator, etc. Make it short and sweet, but not too short so folks
-              don&apos;t simply skip over it entirely.
+              {fetchError && (<p>{fetchError}</p>)}
+              {staff && (
+              <div>
+                {staff.map(staf => (
+                  <p>{staf.name}</p>
+                ))}
+              </div>)}
             </Typography>
-            <Stack
-              sx={{ pt: 4 }}
-              direction="row"
-              spacing={2}
-              justifyContent="center"
-            >
-              <Button variant="contained">Main call to action</Button>
-              <Button variant="outlined">Secondary action</Button>
-            </Stack>
           </Container>
         </Box>
     </ThemeProvider>
